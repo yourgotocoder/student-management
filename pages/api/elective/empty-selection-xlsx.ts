e2fd8fdd3f0e76fa5e7b20b221f08369ac6557e3
Sudarshan Rai <yourgotocoder@gmail.com>
2023-07-11 12:07:07 +0530

    Empty selection data in xlsx

---
+++ pages/api/elective/empty-selection-xlsx.ts
@@ -0,0 +1,72 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { MongoClient } from "mongodb";
+import XLSX from "xlsx";
+
+type Data =
+  | {
+    size?: number;
+    data: { REGNO: number; NAME: string }[] | null;
+    error: boolean;
+    message: string;
+  }
+  | Buffer;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method === "GET") {
+    const client = await MongoClient.connect(
+      process.env.DB_CONNECTION as string
+    );
+    const db = client.db("cse");
+    const collection = db.collection("student-data");
+    const sem = req.query.sem && +req.query.sem;
+    const data = await collection.find().toArray();
+    await client.close();
+
+    const _semData = data.filter((student) => student.CURRENT_SEM === sem);
+    let finalData: { REGNO: number; NAME: string }[] = [];
+    switch (sem) {
+      case 5:
+        finalData = _semData
+          .filter(
+            (student) =>
+              !student.ELECTIVE_SELECTIONS ||
+              (student.ELECTIVE_SELECTIONS &&
+                !student.ELECTIVE_SELECTIONS.ELECTIVE_3)
+          )
+          .map((student) => ({ REGNO: student.REGNO, NAME: student.NAME }));
+        break;
+
+      case 7:
+        finalData = _semData
+          .filter(
+            (student) =>
+              !student.ELECTIVE_SELECTIONS ||
+              (student.ELECTIVE_SELECTIONS &&
+                !student.ELECTIVE_SELECTIONS.ELECTIVE_7)
+          )
+          .map((student) => ({ REGNO: student.REGNO, NAME: student.NAME }));
+        break;
+    }
+    const workbook = XLSX.utils.book_new();
+    const worksheet = XLSX.utils.json_to_sheet(finalData);
+    XLSX.utils.book_append_sheet(workbook, worksheet, `${sem}th_MissingData`);
+    const buffer = XLSX.write(workbook, { type: "array", bookType: "xlsx" });
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename=${sem}th_MissingData.xlsx`
+    );
+    res.setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    res.send(Buffer.from(buffer));
+  } else {
+    res
+      .status(405)
+      .json({ data: null, error: true, message: "Invalid request" });
+  }
+}
